3ccdc6f438a637deac3e8e212764097fa622905d
kyrsjo <>
2006-04-07 20:11:14 +0000

    Some more meat on the bone of it, some testing code to test the channelmap holding classes and other classes using this, simulating a real data storage system, etc.

--- CVSROOT/src/org/kyrsjo/givtv/gnomeFrontend/Givtv.java
+++ CVSROOT/src/org/kyrsjo/givtv/gnomeFrontend/Givtv.java
@@ -1,8 +1,8 @@
 package org.kyrsjo.givtv.gnomeFrontend;
 
 import org.gnu.gtk.*;
-import org.gnu.gtk.event.*;
-import org.gnu.gnome.App;
+//import org.gnu.gtk.event.*;
+//import org.gnu.gnome.App;
 import org.gnu.gnome.Program;
 
 /**
@@ -13,12 +13,24 @@ public class Givtv {
 	// Note to self: eclipse run is shift-alt-x, not control-shift-alt-x
 	
 	public static final String version = "0.1";
+	private static ChannelMap channelmap;
 	
 	public static void main(String[] args) {
 		Program.initGnomeUI("gIVTV", version, args);
-		MainWindow mainwindow = new MainWindow();
+		
+		//channellist init
+		channelmap = new ChannelMap();
+		//TODO: Read in channels from gconf (or something...). This is testing code!
+		channelmap.addChannel(new Channel("0", "E9", "NRK1"));
+		channelmap.addChannel(new Channel("0", "E8", "NRK2"));
+		channelmap.addChannel(new Channel("0", "E10", "Tv2"));
+		
+		//Mainwindow init
+		MainWindow mainwindow = new MainWindow(channelmap);
 		mainwindow.show();
+		
 		Gtk.main();
 	}
+	
 }
 
